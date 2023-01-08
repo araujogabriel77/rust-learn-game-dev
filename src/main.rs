3cbb682ef5f23df3147e7ea17e1813f5a8fde308
Gabriel Araújo <gabriel.ferreira@al.senai.br>
2023-01-08 15:35:42 -0300

    feat: Adding SDL2-Image and render a image

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,66 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
+
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    canvas.copy(texture, None, None)?;
+
+    canvas.present();
+
+    Ok(())
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG);
+
+    let window = video_subsystem
+        .window("learn rust sdl2", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
-    let mut canvas = window.into_canvas().build()
+    let mut canvas = window
+        .into_canvas()
+        .build()
         .expect("could not make a canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/bardo.png")?;
+
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
+
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'running;
-                },
+                }
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        //update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+
+        //Time management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 
